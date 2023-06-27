3311dadf8cfeac2bf9245030378109cff6ec671f
Lore1ei <harubator@gmail.com>
2023-06-27 15:47:16 +0700

    fields

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import {ReactNode, useRef} from "react";
+import React, {ReactNode, useCallback, useRef} from "react";
 import {useRouter} from "next/navigation";
+import Image from "next/image";
 
 const Modal = ({children}: {children: ReactNode}) => {
     const overlay = useRef<HTMLDivElement>(null);
@@ -9,8 +10,24 @@ const Modal = ({children}: {children: ReactNode}) => {
 
     const router = useRouter();
 
-    return <div ref={overlay}>
-        <div>
+    const onDismiss = useCallback(() => {
+        router.push('/')
+    }, [router])
+
+    const handleClick = useCallback((e: React.MouseEvent) => {
+        if((e.target === overlay.current) && onDismiss){
+            onDismiss();
+        }
+    }, [onDismiss, router])
+
+    return <div ref={overlay} className='modal' onClick={handleClick}>
+        <button type='button' onClick={onDismiss} className='absolute top-4 right-8'>
+            <Image src='/close.svg'
+                   width={17}
+                   height={17}
+                   alt='close' />
+        </button>
+        <div ref={wrapper} className='modal_wrapper'>
             {children}
         </div>
     </div>
