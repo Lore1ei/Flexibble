cd5299deb1d6f6c669d7f3f349a4f9890cfd1ae0
Lore1ei <harubator@gmail.com>
2023-06-27 17:23:04 +0700

    start fetching

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,8 +1,11 @@
 "use client"
 import {SessionInterface} from "@/common.types";
-import React, {ChangeEvent} from "react";
+import React, {ChangeEvent, useState} from "react";
 import Image from "next/image";
 import FormField from "@/components/FormField";
+import {categoryFilters} from "@/constants";
+import CustomMenu from "@/components/CustomMenu";
+import Button from "@/components/Button"
 
 type Props = {
     type: string;
@@ -15,19 +18,43 @@ const ProjectForm = ({type, session}: Props) => {
 
     }
 
-
     const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
 
-    }
+        const file = e.target.files?.[0];
+
+        if (!file) return;
+
+        if (!file.type.includes('image')) {
+            alert('Please upload an image!');
+
+            return;
+        }
+
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onload = () => {
+            const result = reader.result as string;
+
+            handleStateChange("image", result)
+        };
+    };
 
     function handleStateChange(fieldName: string, value: string) {
-
+        setForm((prevState) => ({...prevState, [fieldName]: value}))
     }
 
-    const form = {
-        image: '',
-        title: ''
-    }
+    const [isSubmitting, setIsSubmitting] = useState(false);
+   const [form, setForm] = useState({
+       title: '',
+       description: '',
+       image: '',
+       liveSiteUrl: '',
+       githubUrl: '',
+       category: ''
+   })
 
     return (
         <form onSubmit={handleFormSubmit}
@@ -38,12 +65,13 @@ const ProjectForm = ({type, session}: Props) => {
                 <label htmlFor="poster" className='flexCenter form_image-label'>
                     {!form.image && 'Choose a poster for your project'}
                 </label>
-                <input id='image'
-                       type="text"
-                       accept='image/*'
-                       required={type === 'create'}
-                       className='form_image-input'
-                       onChange={handleChangeImage}
+                <input
+                    id="image"
+                    type="file"
+                    accept='image/*'
+                    required={type === "create"}
+                    className="form_image-input"
+                    onChange={(e) => handleChangeImage(e)}
                 />
                 {form.image && (
                     <Image
@@ -56,11 +84,51 @@ const ProjectForm = ({type, session}: Props) => {
             </div>
 
             <FormField
-                title='Title'
+                title="Title"
                 state={form.title}
-                placeholder='Flexibble'
+                placeholder="Flexibble"
                 setState={(value) => handleStateChange('title', value)}
             />
+
+            <FormField
+                title='Description'
+                state={form.description}
+                placeholder="Showcase and discover remarkable developer projects."
+                isTextArea
+                setState={(value) => handleStateChange('description', value)}
+            />
+
+            <FormField
+                type="url"
+                title="Website URL"
+                state={form.liveSiteUrl}
+                placeholder="your.site.com"
+                setState={(value) => handleStateChange('liveSiteUrl', value)}
+            />
+
+            <FormField
+                type="url"
+                title="GitHub URL"
+                state={form.githubUrl}
+                placeholder="https://github.com/Lore1ei"
+                setState={(value) => handleStateChange('githubUrl', value)}
+            />
+
+            <CustomMenu
+                title='Category'
+                state={form.category}
+                filters={categoryFilters}
+                setState={(value) => handleStateChange('category', value)}
+            />
+
+            <div className='flexStart w-full'>
+                <Button
+                    title={isSubmitting ? `${type === "create" ? "Creating" : "Editing"}` : `${type === "create" ? "Create" : "Edit"}`}
+                    type="submit"
+                    leftIcon={isSubmitting ? "" : "/plus.svg"}
+                    submitting={isSubmitting}
+                />
+            </div>
         </form>
     )
 }
